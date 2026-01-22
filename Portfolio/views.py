6d2a5961529807248baa0743480737769e8e788a
Erick-Kariuki <ithandierick4@gmail.com>
2026-01-23 00:02:42 +0300

    Prepare Django portfolio for deployment

---
+++ Portfolio/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from Portfolio.forms import ContactForm
+
+def index(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+                subject=form.cleaned_data['subject'],
+                message=f"""
+Name: {form.cleaned_data['name']}
+Email: {form.cleaned_data['email']}
+
+Message:
+{form.cleaned_data['message']}
+                """,
+                from_email=None,
+                recipient_list=['ithandierick4@gmail.com'],
+            )
+
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': True})
+
+            form = ContactForm()  # reset after success
+
+    else:
+        form = ContactForm()
+
+    return render(request, 'index.html', {'form': form})
+
+
+def mydairyfarm(request):
+    return render(request, 'mydairyfarm.html')
+def iothealthsystem(request):
+    return render(request, 'iothealthsystem.html')
+def weatherapp(request):
+    return render(request, 'weatherapp.html')
